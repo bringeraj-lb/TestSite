02297a3173ba2f743bced6a188af1878699c45fa
Pavan <pavans.shetty.2452@gmail.com>
2021-06-21 22:17:27 +0530

    working login

--- asset/js/login.js
+++ asset/js/login.js
@@ -1,18 +1,8 @@
 
-//  function validateform1(){
-     
-//      var id=document.loginform.id.value;
-//      var password=document.loginform.password.value;
-//      console.log(id,password);
 
-// if(document.getElementById("id").value == 's1' && document.getElementById("pw").value == '1234' ){
-//         alert("Login Successful");
-//         window.location.replace("http://www.google.com/");
-//     }
-//  }
  function validateform() { 
-     let id=document.loginform.id.value;
-     let password=document.loginform.password.value;
+     let id=document.getElementById("id").value;
+     let password=document.getElementById("pw").value;
      console.log(id,password);
 
      if(id==" " || password==" ")
@@ -21,34 +11,23 @@
      }
      else
      {
-        //  if(id=="s1" && password=="1234")
-        //  { 
-        //      console.log("i am in 250");
-        //      window.location.replace("");
-        //  }
-         
-        //  else
-        //  {
-        //      alert("Invalid credentials");
-        //  }
-
-    
+        
          switch(id)
          {
              case "royal250": if(password=="Lb14Xyd!"){
-                 alert("valid student");
-                window.location.replace("http://www.w3schools.com");
+                 
+                window.location.replace("./Demopages/student.html");
              }
              
              case "t-royal30": if(password=="Lbvse@15"){
-                  alert("valid teacher");
-                 window.location.replace("http://www.w3schools.com");
+                  
+                 window.location.replace("./Demopages/teacher.html");
              }
              return;
              case "t-royal1": if(password=="Lbafs32!"){
-                  alert("valid school");
-                //  window.location.replace("http://www.w3schools.com");
-                window.location.href="teacher.html";
+                  
+                
+                window.location.replace("./Demopages/school.html");
              }
              return;
              default : alert("Invalid credentials");
